79b5df9982627085415267bfc43c23a92cc09446
Fragonite <@>
2023-06-18 03:53:46 +0800

    Insert CSV data into database.

--- user_upload.php
+++ user_upload.php
@@ -12,7 +12,7 @@ $skip_invalid = false;
 
 set_exception_handler("exception_handler");
 
-$args = getopt("d:h:u:p:", ["file:", "create_table", "dry_run", "help"]);
+$args = getopt("d:h:u:p:", ["file:", "create_table", "dry_run", "help", "no_log", "skip_invalid"]);
 // var_dump($args);
 
 $help_output = "
@@ -36,18 +36,21 @@ Arguments:
     --help            Show this help message.
 ";
 
+if (isset($args["no_log"])) {
+    $no_log = true;
+} else {
+    set_exception_handler("exception_handler_with_log");
+}
 if (isset($args["help"])) {
     echo $help_output;
     exit(0);
 }
-
 if (isset($args["d"])) {
     $sql_db = $args["d"];
 } else {
     echo $help_output;
     exit(1);
 }
-
 if (isset($args["h"])) {
     $sql_host = $args["h"];
 }
@@ -57,9 +60,6 @@ if (isset($args["u"])) {
 if (isset($args["p"])) {
     $sql_pass = $args["p"];
 }
-// if (isset($args["no_log"])) {
-//     $no_log = true;
-// }
 if (isset($args["skip_invalid"])) {
     $skip_invalid = true;
 }
@@ -69,14 +69,10 @@ if (isset($args["create_table"])) {
 }
 
 if ($create_table) {
-    // try {
     $conn = new mysqli($sql_host, $sql_user, $sql_pass, $sql_db);
-    // } catch (Exception $e) {
-    //     echo "Error: " . $e->getMessage() . "\n";
-    //     exit(1);
-    // }
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        fwrite(STDERR, "Error: Connection failed: " . $conn->connect_error . "\n");
+        exit(1);
     }
 
     $sql = "CREATE TABLE IF NOT EXISTS users (
@@ -125,19 +121,28 @@ if (isset($args["file"])) {
 
     $line_number = 2; // We already read line 1 (index 0)
     $lines_read = 0;
+    $data = [];
     while (($row = fgetcsv($file_pointer)) !== false) {
         if (count($row) !== 3) {
-            fwrite(STDERR, "Error: Expected three columns on line $line_number\n");
+            fwrite(STDERR, "Error: Expected three columns on line $line_number.\n");
+            if ($skip_invalid) {
+                echo "Skipping line $line_number.\n";
+                $line_number++;
+                continue;
+            }
             exit(1);
         }
 
         $row = array_map("trim", $row);
-        $row = array_map("strtolower", $row);
-
 
         // Check if email is valid
         if (!filter_var($row[2], FILTER_VALIDATE_EMAIL)) {
             fwrite(STDERR, "Error: Invalid email address on line $line_number: $row[2]\n");
+            if ($skip_invalid) {
+                echo "Skipping line $line_number.\n";
+                $line_number++;
+                continue;
+            }
             exit(1);
         }
 
@@ -146,13 +151,60 @@ if (isset($args["file"])) {
         // https://www.kalzumeus.com/2010/06/17/falsehoods-programmers-believe-about-names/
         $row[0] = ucfirst($row[0]);
         $row[1] = ucfirst($row[1]);
+        $data[] = $row;
         $line_number++;
+        $lines_read++;
     }
+
+    fclose($file_pointer);
+    if ($lines_read === 0) {
+        fwrite(STDERR, "Error: No valid data found in $file.\n");
+        exit(1);
+    }
+    echo "File read successfully. $lines_read lines read.\n";
+
+    foreach ($data as $entry) {
+        echo "Inserting $entry[0] $entry[1] $entry[2] into database.\n";
+    }
+
+    $conn = new mysqli($sql_host, $sql_user, $sql_pass, $sql_db);
+    if ($conn->connect_error) {
+        fwrite(STDERR, "Error: Connection failed: " . $conn->connect_error . "\n");
+        exit(1);
+    }
+    $statement = $conn->prepare("INSERT INTO users (name, surname, email) VALUES (?, ?, ?)");
+    $entries_inserted = 0;
+    foreach ($data as $entry) {
+        $statement->bind_param("sss", $entry[0], $entry[1], $entry[2]);
+        try {
+            $statement->execute();
+        } catch (Exception $e) {
+            fwrite(STDERR, "Error: " . $e->getMessage() . "\n");
+            echo "Skipping.\n";
+            $entries_inserted--;
+        }
+        $entries_inserted++;
+    }
+    $statement->close();
+    $conn->close();
+    if ($entries_inserted === 0) {
+        fwrite(STDERR, "Error: No entries inserted.\n");
+        exit(1);
+    }
+    echo "$entries_inserted entries inserted successfully.\n";
+    exit(0);
 }
-function exception_handler($exception)
+
+function exception_handler_with_log($exception)
 {
     $timestamp = date("Y-m-d H:i:s");
     error_log("[$timestamp] " . $exception . "\n", 3, "user_upload.log");
+    fwrite(STDERR, "Error: " . $exception->getMessage() . "\n");
+    exit(1);
+}
+
+function exception_handler($exception)
+{
     fwrite(STDERR, "Error: " . $exception->getMessage() . "\n");
     exit(1);
 }
