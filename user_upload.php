5626c27e2568bbbcb9a3e2cbec1662c9badaf0e3
Fragonite <@>
2023-06-18 19:33:15 +0800

    Refactor.

--- user_upload.php
+++ user_upload.php
@@ -1,21 +1,20 @@
-<?php
+<?php {
+    $sql_host = "localhost";
+    $sql_user = get_current_user();
+    $sql_pass = null;
+    $sql_db = "";
+    $create_table = false;
+    $file = null;
+    $dry_run = false;
+    $no_log = false;
+    $skip_invalid = false;
 
-$sql_host = "localhost";
-$sql_user = get_current_user();
-$sql_pass = null;
-$sql_db = "";
-$create_table = false;
-$file = null;
-$dry_run = false;
-$no_log = false;
-$skip_invalid = false;
+    set_exception_handler("exception_handler");
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-set_exception_handler("exception_handler");
+    $args = getopt("d:h:u:p:", ["file:", "create_table", "dry_run", "help", "no_log", "skip_invalid"]);
 
-$args = getopt("d:h:u:p:", ["file:", "create_table", "dry_run", "help", "no_log", "skip_invalid"]);
-// var_dump($args);
-
-$help_output = "
+    $help_output = "
 Description:
     This example script can create and insert data into a table called \"users\".
     It takes a CSV file with three columns: name, surname, email.
@@ -36,57 +35,112 @@ Arguments:
     --help            Show this help message.
 ";
 
-if (isset($args["no_log"])) {
-    $no_log = true;
-} else {
-    set_exception_handler("exception_handler_with_log");
-}
-if (isset($args["help"])) {
-    echo $help_output;
-    exit(0);
-}
-if (isset($args["d"])) {
-    $sql_db = $args["d"];
-} else {
-    echo $help_output;
-    exit(1);
-}
-if (isset($args["h"])) {
-    $sql_host = $args["h"];
-}
-if (isset($args["u"])) {
-    $sql_user = $args["u"];
-}
-if (isset($args["p"])) {
-    $sql_pass = $args["p"];
-}
-if (isset($args["create_table"])) {
-    $create_table = true;
-}
-if (isset($args["dry_run"])) {
-    $dry_run = true;
-}
-if (isset($args["skip_invalid"])) {
-    $skip_invalid = true;
-}
-
-if ($create_table) {
-    $conn = new mysqli($sql_host, $sql_user, $sql_pass, $sql_db);
-    if ($conn->connect_error) {
-        fwrite(STDERR, "Error: Connection failed: " . $conn->connect_error . "\n");
+    if (isset($args["no_log"])) {
+        $no_log = true;
+    } else {
+        set_exception_handler("exception_handler_with_log");
+    }
+    if (isset($args["help"])) {
+        echo $help_output;
+        exit(0);
+    }
+    if (isset($args["d"])) {
+        $sql_db = $args["d"];
+    } else {
+        echo $help_output;
         exit(1);
     }
+    if (isset($args["h"])) {
+        $sql_host = $args["h"];
+    }
+    if (isset($args["u"])) {
+        $sql_user = $args["u"];
+    }
+    if (isset($args["p"])) {
+        $sql_pass = $args["p"];
+    }
+    if (isset($args["create_table"])) {
+        $create_table = true;
+    }
+    if (isset($args["dry_run"])) {
+        $dry_run = true;
+    }
+    if (isset($args["skip_invalid"])) {
+        $skip_invalid = true;
+    }
 
-    $result = $conn->query("SHOW TABLES LIKE 'users'");
-    if ($result && $result->num_rows > 0) {
-        $sql = "TRUNCATE TABLE users";
-        if ($conn->query($sql) === false) {
-            echo "Error truncating table: " . $conn->error . "\n";
-            $conn->close();
+    $connection = null;
+
+    if ($create_table) {
+        if ($connection === null) {
+            $connection = connect_to_database($sql_host, $sql_user, $sql_pass, $sql_db);
+        }
+        try {
+            rebuild_users_table($connection);
+        } catch (Exception $e) {
+            fwrite(STDERR, "Error: " . $e->getMessage() . "\n");
+            $connection->close();
             exit(1);
         }
+    }
 
+    if (isset($args["file"])) {
+        $file = $args["file"];
+        if ($connection === null && !($dry_run)) {
+            $connection = connect_to_database($sql_host, $sql_user, $sql_pass, $sql_db);
+        }
+
+        try {
+            insert_users_from_csv($connection, $file, $dry_run, $skip_invalid);
+        } catch (Exception $e) {
+            fwrite(STDERR, "Error: " . $e->getMessage() . "\n");
+            if ($connection !== null) {
+                $connection->close();
+                exit(1);
+            }
+        }
+    }
+
+    if ($connection !== null) {
+        $connection->close();
+    }
+}
+
+function exception_handler_with_log($exception)
+{
+    $timestamp = date("Y-m-d H:i:s");
+    error_log("[$timestamp] " . $exception . "\n", 3, "user_upload.log");
+    fwrite(STDERR, "Error: " . $exception->getMessage() . "\n");
+    exit(1);
+}
+
+function exception_handler($exception)
+{
+    fwrite(STDERR, "Error: " . $exception->getMessage() . "\n");
+    exit(1);
+}
+
+function connect_to_database($host, $user, $pass, $db)
+{
+    $conn = new mysqli($host, $user, $pass, $db);
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
+}
+
+function rebuild_users_table($connection)
+{
+    $result = $connection->query("SHOW TABLES LIKE 'users'");
+
+    if ($result && $result->num_rows > 0) {
+        $sql = "TRUNCATE TABLE users";
+        try {
+            $connection->query($sql);
+        } finally {
+        }
         echo "Table truncated successfully.\n";
+
     } else {
 
         $sql = "CREATE TABLE IF NOT EXISTS users (
@@ -95,21 +149,17 @@ if ($create_table) {
             surname VARCHAR(30) NOT NULL,
             email VARCHAR(50) NOT NULL UNIQUE
         )";
-
-        if ($conn->query($sql) === false) {
-            echo "Error creating table: " . $conn->error . "\n";
-            $conn->close();
-            exit(1);
+        try {
+            $connection->query($sql);
+        } finally {
         }
-
         echo "Table created successfully.\n";
+
     }
-    $conn->close();
 }
 
-if (isset($args["file"])) {
-    $file = $args["file"];
-
+function insert_users_from_csv($connection, $file, $dry_run, $skip_invalid)
+{
     if (!(is_readable($file))) {
         fwrite(STDERR, "Error: File does not exist or is not readable.\n");
         exit(1);
@@ -172,21 +222,16 @@ if (isset($args["file"])) {
         $line_number++;
         $lines_read++;
     }
-
     fclose($file_pointer);
+
     if ($lines_read === 0) {
         fwrite(STDERR, "Error: No valid data found in $file.\n");
         exit(1);
     }
-    echo "File read successfully. $lines_read lines read.\n";
+    echo "File read successfully ($lines_read lines read).\n";
 
     if (!($dry_run)) {
-        $conn = new mysqli($sql_host, $sql_user, $sql_pass, $sql_db);
-        if ($conn->connect_error) {
-            fwrite(STDERR, "Error: Connection failed: " . $conn->connect_error . "\n");
-            exit(1);
-        }
-        $statement = $conn->prepare("INSERT INTO users (name, surname, email) VALUES (?, ?, ?)");
+        $statement = $connection->prepare("INSERT INTO users (name, surname, email) VALUES (?, ?, ?)");
         $entries_inserted = 0;
         foreach ($data as $entry) {
             $statement->bind_param("sss", $entry[0], $entry[1], $entry[2]);
@@ -199,26 +244,10 @@ if (isset($args["file"])) {
             }
         }
         $statement->close();
-        $conn->close();
         if ($entries_inserted === 0) {
             fwrite(STDERR, "Error: No entries inserted.\n");
             exit(1);
         }
         echo "$entries_inserted entries inserted successfully.\n";
     }
-    exit(0);
-}
-
-function exception_handler_with_log($exception)
-{
-    $timestamp = date("Y-m-d H:i:s");
-    error_log("[$timestamp] " . $exception . "\n", 3, "user_upload.log");
-    fwrite(STDERR, "Error: " . $exception->getMessage() . "\n");
-    exit(1);
-}
-
-function exception_handler($exception)
-{
-    fwrite(STDERR, "Error: " . $exception->getMessage() . "\n");
-    exit(1);
 }
